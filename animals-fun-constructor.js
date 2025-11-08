db66ad9021728fd749f2a328adc5e2cfc6ae1b0a
miwanoff <marina.iva.noffa@gmail.com>
2025-11-08 12:36:14 +0200

    Add 'Кролик'

--- animals-fun-constructor.js
+++ animals-fun-constructor.js
@@ -2,3 +2,10 @@ function Animal(name) {
   this.name = name;
   this.speed = 0;
 }
+
+Animal.prototype.run = function () {
+  alert(this.name + " біжить!");
+};
+
+let an = new Animal('Кролик');
+an.run();
